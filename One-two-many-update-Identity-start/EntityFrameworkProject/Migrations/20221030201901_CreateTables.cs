76580a3b7144dbd1e213e2927340f1c36233d20c
Azer444 <azerrh@code.edu.az>
2022-10-31 00:24:06 +0400

    OneTwoOne

---
+++ One-two-many-update-Identity-start/EntityFrameworkProject/Migrations/20221030201901_CreateTables.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkProject.Migrations
+{
+    public partial class CreateTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 31, 0, 19, 1, 115, DateTimeKind.Local).AddTicks(7746));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 31, 0, 19, 1, 116, DateTimeKind.Local).AddTicks(5928));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 31, 0, 19, 1, 116, DateTimeKind.Local).AddTicks(5955));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 28, 17, 3, 50, 465, DateTimeKind.Local).AddTicks(9416));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 28, 17, 3, 50, 466, DateTimeKind.Local).AddTicks(9909));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 28, 17, 3, 50, 466, DateTimeKind.Local).AddTicks(9940));
+        }
+    }
+}
